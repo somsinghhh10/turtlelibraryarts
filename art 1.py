5ed164576dcbb95018fcb7685edba71a5d0b9184
Developer Som <somisbest2011@gmail.com>
2025-08-24 12:26:34 +0530

    art 1.py

---
+++ art 1.py
@@ -0,0 +1,26 @@
+import turtle
+import colorsys
+
+def draw_hypnotic_art():
+    screen = turtle.Screen()
+    screen.bgcolor("black")
+
+    t = turtle.Turtle()
+    t.speed(0)
+    t.width(2)
+    t.hideturtle()
+
+    num_colors = 72
+    colors = [colorsys.hsv_to_rgb(i/num_colors, 1.0, 1.0) for i in range(num_colors)]
+
+    for i in range(360):
+        color = colors[i % num_colors]
+        t.pencolor(color[0], color[1], color[2])
+        t.circle(i * 0.5)
+        t.right(45)
+        t.forward(i * 0.2)
+        t.right(45)
+
+    screen.mainloop()
+
+draw_hypnotic_art()
